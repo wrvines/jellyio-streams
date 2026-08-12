6a9576987a9e4c44d8d7bb6ac5c6a8f8f4bb8075
wrvines <wrvines@users.noreply.github.com>
2026-08-11 20:33:44 -0600

    test: scaffold xunit test project

---
+++ tests/Jellyfin.Plugin.AIOStreams.Tests/PlaceholderTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Jellyfin.Plugin.AIOStreams.Tests;
+
+public class PlaceholderTests
+{
+    [Fact]
+    public void Placeholder_AlwaysPasses()
+    {
+        Assert.True(true);
+    }
+}
